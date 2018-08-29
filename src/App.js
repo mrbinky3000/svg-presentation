12d2da33561c5749dd525ba558df4416058e5faf
Matthew Toledo <mtoledo@express.com>
2018-08-28 22:26:11 -0400

    Add grid of circles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import './App.css';
 
+const Grid = () => {
+  const rows = [];
+  let i = 0;
+
+  for(let y = 50; y < 800; y += 100) {
+    const columns = [];
+    for(let x = 50; x < 800; x += 100) {
+      columns.push(
+        <circle className="waveCircle" id={`waveCircle${i}`} key={i} cx={x} cy={y} r={25} stroke={10} />
+      );
+      i += 1;
+    }
+    rows.push(columns);
+  }
+  return rows;
+}
+
 class App extends React.Component {
   render() {
     return (
@@ -12,6 +29,7 @@ class App extends React.Component {
           xmlns="http://www.w3.org/2000/svg"
         >
           <rect width="100%" height="100%" fill="#efefef"/>
+          <Grid />
         </svg>
       </div>
     );
